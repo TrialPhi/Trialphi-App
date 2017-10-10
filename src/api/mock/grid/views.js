5a8411b2033b61ec70a28d9c2f60cb155b880034
TrialPhi <RYAN.HENDRICKS@baruchmail.cuny.edu>
2017-10-10 01:23:21 -0400

    Removed unused files

--- src/api/mock/grid/views.js
+++ src/api/mock/grid/views.js
@@ -1,12 +1,12 @@
-/* eslint-disable global-require */
+/* eslint-disable global-require
 import hermsAutomatedValves from './herms-automated-valves';
 import hermsManualValves from './herms-manual-valves';
 import hermsHoseSwap from './herms-hose-swap';
 import fermentation from './fermentation';
 import rimsBiab from './rims-biab';
-
+*/
 const views = {
-  'herms-automated-valves': hermsAutomatedValves,
+/* 'herms-automated-valves': hermsAutomatedValves,
   'herms-manual-valves': hermsManualValves,
   'herms-hose-swap': hermsHoseSwap,
   'rims-biab': rimsBiab,
@@ -17,7 +17,7 @@ const views = {
   view4: require('./test_data/view4.json'),
   view5: require('./test_data/view5.json'),
   coilTest: require('./test_data/coil_test.json'),
-  valveTeeTest: require('./test_data/valve_tee_test.json'),
+  valveTeeTest: require('./test_data/valve_tee_test.json'), */
 };
 
 export default views;
